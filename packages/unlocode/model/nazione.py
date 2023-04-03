fce9f32097098689f320a83620507956cfa6fa13
tommaso <codicem69@gmail.com>
2023-04-03 12:18:54 +0200

    aggiunto formulaColumn per avere il code e il nome assieme

--- packages/unlocode/model/nazione.py
+++ packages/unlocode/model/nazione.py
@@ -3,7 +3,8 @@
 
 class Table(object):
     def config_db(self, pkg):
-        tbl =  pkg.table('nazione',pkey='code',name_long='nazione',name_plural='nazioni',caption_field='code')
+        tbl =  pkg.table('nazione',pkey='code',name_long='nazione',name_plural='nazioni',caption_field='codename')
         self.sysFields(tbl,id=False)
         tbl.column('code',size='2',name_long='!![en]code',name_short='!![en]code')
         tbl.column('nome',name_long='!![en]name',name_short='!![en]name')
+        tbl.formulaColumn('codename',"$code || ' - ' || $nome")
