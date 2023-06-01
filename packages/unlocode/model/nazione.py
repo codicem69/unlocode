eafda13a0f8a4eb96d78b3d5631ff43bd08d4918
tommaso <codicem69@gmail.com>
2023-06-01 11:27:03 +0200

    aggiunto pyColumn ue_san per verifica stato ue e preferenza shipsteps

--- packages/unlocode/model/nazione.py
+++ packages/unlocode/model/nazione.py
@@ -9,3 +9,19 @@ class Table(object):
         tbl.column('nome',name_long='!![en]name',name_short='!![en]name')
         tbl.column('ue', dtype='B', name_short='!![en]UE')
         tbl.formulaColumn('codename',"$code || ' - ' || $nome")
+        tbl.pyColumn('ue_san',name_long='!![en]UE Sanimare',dtype='B', static=True)
+        #pyColumn ue_san creata per verificare tramite le preferenze del pkg shipsteps se abilitare o no le pratiche sanimare
+        #alle provenienze navi da tutti gli stati o solo quelli extra ue
+        #nella th_arrival di shipsteps ho inserito nei widget sanimare della task list la variabile hidden che verifica il valore ue_san
+        #in caso di valore true nasconde il widget
+        
+    def pyColumn_ue_san(self, record, field):
+        pref=self.db.application.getPreference('ue',pkg='shipsteps')
+        code=record['code']
+        if pref is False:
+            uesan = self.db.table('unlocode.nazione').readColumns(columns="""CASE WHEN $ue is True THEN true ELSE false END""",
+                                                                where="$code=:code", code = code)    
+        else:
+            uesan = self.db.table('unlocode.nazione').readColumns(columns="""CASE WHEN $ue is True THEN false ELSE false END""",
+                                                                where="$code=:code", code = code)  
+        return uesan
